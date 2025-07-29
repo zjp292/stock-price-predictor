c3f6c04aa2629298c758abbf49671a45d927c649
Zachary Parham <zjp29@nau.edu>
2025-07-28 20:17:31 -0700

    updated data to be s&p 500 for further depth

--- main.py
+++ main.py
@@ -17,46 +17,44 @@ API_KEY = os.getenv("TIINGO_API_KEY")
 config = {"api_key": API_KEY, "session": True}
 
 client = TiingoClient(config)
-ticker = "AAPL"
+ticker = "VOO"
 
-if not os.path.isfile("aapl_hist_data.csv"):
+if not os.path.isfile("sp500.csv"):
     print("not executing")
-    aapl_hist_data = client.get_dataframe(ticker, "2020-01-01")
+    aapl_hist_data = client.get_dataframe(ticker, "1999-01-01")
     aapl_hist_data.index = pd.to_datetime(aapl_hist_data.index).tz_localize(None)
-    aapl_hist_data.to_csv("aapl_hist_data.csv", index=True)
+    aapl_hist_data.to_csv("s&p500_data.csv", index=True)
 
-apple_stock_df = pd.read_csv("aapl_hist_data.csv")
-apple_stock_df = apple_stock_df.iloc[365:]  # this is to remove the covid outlier data
+sp500_df = pd.read_csv("sp500.csv")
 
-apple_stock_df["seven_day_rolling_avg"] = (
-    apple_stock_df["close"].rolling(window=7).mean()
-)
-apple_stock_df["thirty_day_rolling_avg"] = (
-    apple_stock_df["close"].rolling(window=30).mean()
-)
-apple_stock_df["return"] = apple_stock_df["close"].pct_change()
-apple_stock_df["SMA"] = apple_stock_df["close"].rolling(window=5).mean()
-apple_stock_df["EMA"] = apple_stock_df["close"].ewm(span=5).mean()
-apple_stock_df["CMA"] = apple_stock_df["close"].expanding().mean()
-apple_stock_df["lag1"] = apple_stock_df["close"].shift(1)
-apple_stock_df["target"] = apple_stock_df["lag1"] > apple_stock_df["close"].astype(int)
+sp500_df["seven_day_rolling_avg"] = sp500_df["Close"].rolling(window=7).mean()
+sp500_df["thirty_day_rolling_avg"] = sp500_df["Close"].rolling(window=30).mean()
+sp500_df["return"] = sp500_df["Close"].pct_change()
+sp500_df["Tomorrow"] = sp500_df["Close"].shift(-1)
+sp500_df["SMA"] = sp500_df["Close"].rolling(window=5).mean()
+sp500_df["EMA"] = sp500_df["Close"].ewm(span=5).mean()
+sp500_df["CMA"] = sp500_df["Close"].expanding().mean()
+sp500_df["lag1"] = sp500_df["Close"].shift(1)
+sp500_df["Target"] = (sp500_df["Tomorrow"] > sp500_df["Close"]).astype(int)
+sp500_df = sp500_df.loc["1990-01-01":].copy()
 
-features = ["SMA", "EMA", "CMA", "open", "high", "low", "close", "volume"]
+print(sp500_df)
+# features = ["SMA", "EMA", "CMA", "open", "high", "low", "close", "volume"]
 
-apple_stock_df = apple_stock_df.dropna()
+sp500_df = sp500_df.dropna()
 
-plt.plot(apple_stock_df["date"], apple_stock_df["close"], label="Closing Price")
-plt.plot(apple_stock_df["date"], apple_stock_df["SMA"], label="SMA")
-plt.plot(apple_stock_df["date"], apple_stock_df["EMA"], label="EMA")
-plt.plot(apple_stock_df["date"], apple_stock_df["CMA"], label="CMA")
-plt.xlabel("time")
-plt.ylabel("closing price")
-plt.title("aaple stock")
-plt.legend()
-plt.show()
+# plt.plot(sp500_df["date"], sp500_df["close"], label="Closing Price")
+# plt.plot(sp500_df["date"], sp500_df["SMA"], label="SMA")
+# plt.plot(sp500_df["date"], sp500_df["EMA"], label="EMA")
+# plt.plot(sp500_df["date"], sp500_df["CMA"], label="CMA")
+# plt.xlabel("time")
+# plt.ylabel("closing price")
+# plt.title("aaple stock")
+# plt.legend()
+# plt.show()
 
-X = apple_stock_df[["CMA", "SMA", "EMA"]]
-y = apple_stock_df["close"]
+X = sp500_df[["CMA", "SMA", "EMA"]]
+y = sp500_df["Close"]
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
@@ -78,34 +76,50 @@ print(metrics.mean_squared_error(y_test, predictions))  # 3.4416421437571643
 print(np.sqrt(metrics.mean_squared_error(y_test, predictions)))  # 1.8551663385683679
 
 # random forest classifier
-x, y = apple_stock_df[features], apple_stock_df["target"]
-x, y = x[:-1], y[:-1]
-x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=False, test_size=0.2)
+# x, y = sp500_df[features], sp500_df["target"]
+# x, y = x[:-1], y[:-1]
+# x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=False, test_size=0.2)
 
 
 randForClf = RandomForestClassifier(
     n_estimators=100, min_samples_split=100, random_state=1
 )
-randForClf.fit(x_train, y_train)
+# train = sp500_df.iloc[:-100]
+# test = sp500_df.iloc[-100:]
+predictors = ["Close", "Volume", "Open", "High", "Low"]
+# randForClf.fit(train[predictors], train["target"])
 
-y_pred = randForClf.predict(x_test)
-print("accuracy: ", metrics.precision_score(y_test, y_pred))
-test_res = x_test.copy()
+# y_pred = randForClf.predict(x_test)
 
-test_res["Actual"] = y_test.values
-test_res["Predicted"] = y_pred
-test_res = test_res.reset_index()
+# predictions = randForClf.predict(test[predictors])
+# predictions = pd.Series(predictions, index=test.index)
+# print("accuracy: ", metrics.precision_score(test["target"], predictions))
 
-plt.figure(figsize=(15, 5))
-plt.plot(test_res.index, test_res["Actual"], label="Actual", marker="o")
-plt.plot(test_res.index, test_res["Predicted"], label="Predicted", marker="x")
-plt.xlabel("Index")
-plt.ylabel("Target (1 = price dropped)")
-plt.title("Random Forest Classifier: Actual vs Predicted")
-plt.legend()
-plt.grid(True)
-plt.show()
+# combined = pd.concat([test["target"], predictions], axis=1)
+# print(combined.plot())
 
 
-metrics.ConfusionMatrixDisplay.from_estimator(randForClf, x_test, y_test)
-plt.show()
+def predict(train, test, predictors, model):
+    model.fit(train[predictors], train["Target"])
+    preds = model.predict(test[predictors])
+    preds = pd.Series(preds, index=test.index, name="Predictions")
+    combined = pd.concat([test["Target"], preds], axis=1)
+    return combined
+
+
+def backtest(data, model, predictors, start=2500, step=250):
+    all_predictions = []
+
+    for i in range(start, data.shape[0], step):
+        train = data.iloc[0:i].copy()
+        test = data.iloc[i : (i + step)].copy()
+        predictions = predict(train, test, predictors, model)
+        all_predictions.append(predictions)
+
+    return pd.concat(all_predictions)
+
+
+predictions = backtest(sp500_df, randForClf, predictors)
+print(predictions["Predictions"].value_counts())
+print(metrics.precision_score(predictions["Target"], predictions["Predictions"]))
+print(predictions["Target"].value_counts() / predictions.shape[0])
