2db455ca290e8eed560102fdc6bb0c26d8caa4d0
zjp292 <zacharyparham0@gmail.com>
2025-07-31 17:40:18 -0700

    cleaned code

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ sp500_df["Target"] = (sp500_df["Tomorrow"] > sp500_df["Close"]).astype(int)
 sp500_df = sp500_df.loc["1990-01-01":].copy()
 
 print(sp500_df)
-# features = ["SMA", "EMA", "CMA", "open", "high", "low", "close", "volume"]
 
 sp500_df = sp500_df.dropna()
 
@@ -56,9 +55,9 @@ sp500_df = sp500_df.dropna()
 X = sp500_df[["CMA", "SMA", "EMA"]]
 y = sp500_df["Close"]
 
+# linear regression
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# linear regression
 linReg = LinearRegression(fit_intercept=True)
 linReg.fit(x_train, y_train)
 print(linReg.score(X, y))  # 0.995254691136916
@@ -76,28 +75,12 @@ print(metrics.mean_squared_error(y_test, predictions))  # 3.4416421437571643
 print(np.sqrt(metrics.mean_squared_error(y_test, predictions)))  # 1.8551663385683679
 
 # random forest classifier
-# x, y = sp500_df[features], sp500_df["target"]
-# x, y = x[:-1], y[:-1]
-# x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=False, test_size=0.2)
-
 
 randForClf = RandomForestClassifier(
     n_estimators=100, min_samples_split=100, random_state=1
 )
-# train = sp500_df.iloc[:-100]
-# test = sp500_df.iloc[-100:]
+
 predictors = ["Close", "Volume", "Open", "High", "Low"]
-# randForClf.fit(train[predictors], train["target"])
-
-# y_pred = randForClf.predict(x_test)
-
-# predictions = randForClf.predict(test[predictors])
-# predictions = pd.Series(predictions, index=test.index)
-# print("accuracy: ", metrics.precision_score(test["target"], predictions))
-
-# combined = pd.concat([test["target"], predictions], axis=1)
-# print(combined.plot())
-
 
 def predict(train, test, predictors, model):
     model.fit(train[predictors], train["Target"])
