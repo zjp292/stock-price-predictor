328e2eb0ad09ab3c7f1909f22c46b63fe1f2d020
zjp292 <zacharyparham0@gmail.com>
2025-08-11 18:46:53 -0700

    updated environment.yml and readme

---
+++ models/neural_models.py
@@ -0,0 +1,17 @@
+import tensorflow as 
+
+class LSTM:
+    def __init__(self, data):
+        self.data = data
+
+    def train(self):
+        # Implement LSTM training logic here
+        pass
+
+    def predict(self):
+        # Implement LSTM prediction logic here
+        pass
+
+    def evaluate(self):
+        # Implement evaluation logic for LSTM model
+        pass
